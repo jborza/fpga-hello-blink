52e35946cd3c72340ab50d3ff5acd6e5db51bbde
jborza <jborza@gmail.com>
2020-08-02 16:03:53 +0200

    xise project file

--- blink1.vhd
+++ blink1.vhd
@@ -3,19 +3,20 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 
+-- interface description - how does this unit look from the outside
 entity blink1 is
     Port ( clock : in  STD_LOGIC;
            led : out  STD_LOGIC);
 end blink1;
 
+-- implementation of the module
 architecture Behavioral of blink1 is
-
 constant counter_limit : integer := 5000000; --counter limit before blink
-
 signal counter : unsigned (24 downto 0); -- 25 bit counter (going to 33M)
-signal blink : std_logic; --internal signal to remember LED output state
+signal blink : STD_LOGIC; --internal signal to remember LED output state
 begin
 
+-- process to run on every clock cycle
 process(clock)
 	begin
 		if rising_edge(clock) then
@@ -28,7 +29,7 @@ process(clock)
 		end if;
 	end process;
 		
+-- asynchronous assignment
 led <= blink;
 
 end Behavioral;
-
